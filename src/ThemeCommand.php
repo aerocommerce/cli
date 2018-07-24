0ff7c1cf3eaa61a6405cb36be4d897a0c9d9a341
Andrew Clark <andrew.clark@techquity.co.uk>
2018-07-24 12:50:53 +0100

    Adds Theme Creation Command
    Adds the ability to use `aero theme name path?` and have the CLI generate a theme directry and stub fikes in your Aero Commerce project.

---
+++ src/ThemeCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Aero\Cli;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class ThemeCommand extends Command
+{
+    private $input;
+    private $output;
+    private $theme;
+    private $path;
+
+    protected function configure()
+    {
+        $this->setName('theme')
+            ->setDescription('Create a new Aero Commerce them.')
+            ->addArgument('name', InputArgument::REQUIRED)
+            ->addArgument('path', InputArgument::OPTIONAL);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->theme = $input->getArgument('name');
+        $this->path = expand_tilde($input->getArgument('path')) ?? getcwd();
+
+        if ($this->isAeroProject()) {
+            $this->makeThemeDirectories();
+            $this->copyTemplateFiles();
+        }
+    }
+
+    private function isAeroProject()
+    {
+        $composer = collect(json_decode(file_get_contents($this->path . '/composer.json')));
+
+        if ($composer->isEmpty()) {
+            return false;
+        }
+
+        return $composer->filter(function ($value, $key) {
+            return $key == 'require';
+        })->flatMap(function ($value) {
+            return get_object_vars($value);
+        })->has('aerocommerce/framework');
+    }
+
+    protected function makeThemeDirectories(): void
+    {
+        $process = new Process('mkdir aero; cd aero; mkdir themes; cd themes; mkdir ' . $this->theme, $this->path);
+        $process->run();
+    }
+
+    /**
+     * @todo make and move actual stubs.
+     */
+    private function copyTemplateFiles()
+    {
+        $process = new Process('touch welcome.blade.php', $this->path . '/aero/themes/' . $this->theme);
+        $process->run();
+    }
+}
