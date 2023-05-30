284ba2e652883871cf68451cbb5e51fa02a383cf
Tim Spratt <timspratt@hotmail.co.uk>
2023-05-30 14:48:46 +0100

    wip

---
+++ src/Installation/RunConfigureCommandNext.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aero\Cli\Installation;
+
+use Aero\Cli\InstallStep;
+
+class RunConfigureCommandNext extends InstallStep
+{
+    public function install(): void
+    {
+        $command = [
+            PHP_BINARY,
+            "{$this->command->relativePath}/artisan",
+            'aero:configure',
+            '--no-interaction',
+        ];
+
+        $this->runCommand($command);
+    }
+}
