7a854715b09618b935b164e4af5dcca1f9566b04
Callum Payne <hello@calpayne.com>
2025-04-14 10:02:43 +0100

    Use Laravel 12

--- src/Installation/CreateProject.php
+++ src/Installation/CreateProject.php
@@ -10,7 +10,7 @@ class CreateProject extends InstallStep
     {
         $this->command->output->write('Downloading base project...');
 
-        $laravel = $this->command->input->getOption('laravel') ?: '11';
+        $laravel = $this->command->input->getOption('laravel') ?: '12';
 
         if (is_numeric($laravel)) {
             $laravel = "~{$laravel}.0";
